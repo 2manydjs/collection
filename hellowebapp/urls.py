bf3288c10f461c6d702df838215df46f85667c7e
Mark <markpith@gmail.com>
2015-10-23 16:24:37 +0200

    After adding new venv and user registration

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
+from django.contrib.auth.views import (
+        password_reset,
+        password_reset_done,
+        password_reset_confirm,
+        password_reset_complete
+)
 
 urlpatterns = patterns('',
                       url(r'^$', 'collection.views.index', name='home'),
@@ -23,5 +29,10 @@ urlpatterns = patterns('',
                       url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
                       url(r'^privatelabels/(?P<slug>[-\w]+)/$', 'collection.views.privatelabel_detail', name='privatelabel_detail'),
                       url(r'^privatelabels/(?P<slug>[-\w]+)/edit/$', 'collection.views.edit_privatelabel', name='edit_privatelabel'),
+                      url(r'^accounts/', include('registration.backends.simple.urls')),
+                      url(r'^accounts/password_reset/$', password_reset, {'template_name': 'registration/password_reset_form.html'}, name="password_reset"),
+                      url(r'^accounts/password_reset/done/$', password_reset_done, {'template_name': 'registration/password_reset_done.html'}, name="password_reset_done"),
+                      url(r'^accounts/password/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', password_reset_confirm, {'template_name': 'registration/password_reset_confirm.html'}, name="password_reset_confirm"),
+                      url(r'^accounts/password_reset/done/$', password_reset_done, {'template_name': 'registration/password_reset_complete.html'}, name="password_reset_complete"),
                       url(r'^admin/', include(admin.site.urls)),
 )
